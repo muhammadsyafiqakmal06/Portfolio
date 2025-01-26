737ef8d2e5b4c3a3266a833530ab121343924b8c
muhammadsyafiqakmal06 <67745635+muhammadsyafiqakmal06@users.noreply.github.com>
2025-01-26 15:13:41 +0700

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -43,16 +43,6 @@ export default {
           tech: 'CodeIgniter 4, Bootstrap',
           github: 'null',
           demo: 'null'
-        },
-        {
-          id: 5,
-          name: 'Login Freelance OKE GARDEN',
-          imageUrl: 'okegarden1',
-          role: 'Frontend Web Developer',
-          status: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-          tech: 'Laravel 8, Bootstrap',
-          github: 'null',
-          demo: 'null'
         }
       ]
     };
